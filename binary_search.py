ca524d237709e6d5874ed0c7cc8c14eeacdbdc5f
root <root@DESKTOP-O92PPAR.localdomain>
2025-10-30 15:54:48 -0400

    Update to binary_search prgram

--- binary_search.py
+++ binary_search.py
@@ -3,19 +3,24 @@ def binary_search(arr, input_val):
     high = len(arr) - 1
 
     while low <= high:
-        mid = low + (high - low) // 2
+        mid = (low + high) // 2
+        print(f"mid = ({low} + {high}) // 2")
+        print(f"\nmid: {mid}")
         if (arr[mid] == input_val):
+            print("Success")
             return mid
         elif (arr[mid] < input_val):
             low = mid + 1
+            print(f"low: {low}")
         else:
             high = mid - 1
+            print(f"high: {high}")
     return -1
 
 
 def main():
-    array = [2, 3, 4, 10, 40]
-    my_val = 40
+    array = [1, 3, 5, 7]
+    my_val = 7
 
     result = binary_search(array, my_val)
     if result != -1:
